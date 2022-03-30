c80c28bfb6d4002159763ddeb68e5e2814933744
JamesH <jwhusc7@gmail.com>
2022-03-30 13:22:37 -0400

    added form with submition handler which returns data from input

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,3 +1,6 @@
+var userFormEl = document.querySelector("#user-form");
+var nameInputEl = document.querySelector("#username");
+
 var getUserRepos = function(user) {
     fetch("https://api.github.com/users/"+ user + "/repos").then(function(response){
         response.json().then(function(data){
@@ -6,4 +9,17 @@ var getUserRepos = function(user) {
     });
 };
 
-getUserRepos();
+var formSubmitHandler = function(event) {
+    event.preventDefault();
+    // get value from input element
+    var username = nameInputEl.value.trim();
+
+    if (username){
+        getUserRepos(username);
+        nameInputEl.value = "";
+    } else {
+        alert("Please enter a GitHub username");
+    }
+};
+
+userFormEl.addEventListener("submit", formSubmitHandler);
