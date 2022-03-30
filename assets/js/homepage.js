7e4e8025519752414d3242ec88fe7160c3c84ed0
JamesH <jwhusc7@gmail.com>
2022-03-30 12:57:41 -0400

    made getUserRepos function dynamic

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,5 +1,9 @@
-var getUserRepos = function() {
-    fetch("https://api.github.com/users/octocat/repos");
+var getUserRepos = function(user) {
+    fetch("https://api.github.com/users/"+ user + "/repos").then(function(response){
+        response.json().then(function(data){
+            console.log(data)
+        })
+    });
 };
 
 getUserRepos();
