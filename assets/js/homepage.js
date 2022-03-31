845d41c972d43c4988b6f012b023059b4336a931
JamesH <jwhusc7@gmail.com>
2022-03-31 12:38:22 -0400

    added search by language buttons

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -2,6 +2,21 @@ var userFormEl = document.querySelector("#user-form");
 var nameInputEl = document.querySelector("#username");
 var repoContainerEl = document.querySelector("#repos-container");
 var repoSearchTerm = document.querySelector("#repo-search-term");
+var languageButtonsEl = document.querySelector("#language-buttons");
+
+var getFeaturedRepos = function(language) {
+    var apiUrl = "https://api.github.com/search/repositories?q=" + language + "+is:featured&sort=help-wanted-issues";
+
+    fetch(apiUrl).then(function(response) {
+        if (response.ok) {
+            response.json().then(function(data){
+                displayRepos(data.items, language)
+            })
+        } else {
+            alert('Error: GitHub User Not Found');
+        }
+    });
+};
 
 var displayRepos = function(repos, searchTerm) {
     // check if api returned any repos
@@ -80,4 +95,14 @@ var formSubmitHandler = function(event) {
     }
 };
 
+var buttonClickHandler = function(event) {
+    var language = event.target.getAttribute("data-language");
+    if (language) {
+        getFeaturedRepos(language);
+        // clear old content (after becase fetch is async)
+        repoContainerEl.textContent = "";
+    }
+};
+
 userFormEl.addEventListener("submit", formSubmitHandler);
+languageButtonsEl.addEventListener("click", buttonClickHandler);
